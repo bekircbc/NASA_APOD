a6ad3a3c1041206c358d6d76277b0c20789a85c4
Bekir - CDI <b.burakcebecii@gmail.com>
2022-12-31 17:44:50 +0100

    added..

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 axios
   .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
   .then(function (response) {
@@ -6,6 +8,13 @@ axios
     document.getElementById("title").innerHTML = response.data.title;
     document.getElementById("explanation").innerHTML =
       response.data.explanation;
-    document.getElementById("astro-image").src = response.data.hdurl;
     document.getElementById("date").innerHTML = response.data.date;
+
+    if (response.data.hdurl === "String") {
+      document.getElementById("astro-image").src = response.data.hdurl;
+    }
+
+    if (response.data.url === "String") {
+      document.getElementById("astro-video").src = response.data.url;
+    }
   });
